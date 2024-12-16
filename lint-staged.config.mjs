f89d24b90367de50b50b933b0d1402544169ed9f
Jeremy Moseley <jeremy@cortexclick.com>
2024-12-16 10:20:57 -0800

    No need to enforce conventional commit format on commit messages.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   "**/*.{ts?(x),mts}": () => "tsc -p tsconfig.prod.json --noEmit",
-  "*.{js,jsx,mjs,cjs,ts,tsx,mts}": ["pnpm lint:file", "vitest related --run"],
+  "*.{js,jsx,mjs,cjs,ts,tsx,mts}": ["pnpm lint:file"],
   "*.{md,json}": "prettier --write",
 };
 
