0474747b78cf54379c5820a9c91e3274f46b5e6f
Jeremy Moseley <jeremy@cortexclick.com>
2025-01-07 13:51:36 -0800

    chore: fix eslint configs.

--- packages/create-gensx/.eslintrc.js
+++ packages/create-gensx/.eslintrc.js
@@ -4,4 +4,12 @@ export default {
     project: "./tsconfig.json",
     tsconfigRootDir: import.meta.dirname,
   },
+  rules: {
+    "node/shebang": [
+      "error",
+      {
+        convertPath: { "src/*.ts": ["^src/(.+?)\\.ts$", "./dist/$1.js"] },
+      },
+    ],
+  },
 };
