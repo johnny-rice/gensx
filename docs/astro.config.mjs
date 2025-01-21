25c40cea793ebdf626fe3e1640f5e641c59ecf53
Derek Legenzoff <derek@cortexclick.com>
2025-01-21 10:55:18 -0800

    docs: adding openai component reference (#149)
    Adds in the reference for openai components. Also trying to land on a
    good layout for reference pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -59,10 +59,10 @@ export default defineConfig({
         //   label: "Examples",
         //   autogenerate: { directory: "examples" },
         // },
-        // {
-        //   label: "Component reference",
-        //   autogenerate: { directory: "component-reference" },
-        // },
+        {
+          label: "Component reference",
+          autogenerate: { directory: "component-reference" },
+        },
       ],
     }),
     tailwind({
