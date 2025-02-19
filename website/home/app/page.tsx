051292d9e7ee911086973a877862ab4c754c6320
Dan Hernandez <33551757+danfhernandez@users.noreply.github.com>
2025-02-19 11:53:15 -0500

    chore: Fix stuttering on home page when scrolled down and choosing examples (#265)
    Fixes #269 
    
    On the home page, when slightly scrolled down, if you hover on an
    example it stutters back and forth in a loop.
    
    Turns out this was caused by height of the website changing between
    examples. One of the examples is longer than the others and when viewing
    it, it increases the total height of the page. So when going back and
    forth it causes the screen to shake.
    
    The fix was simply add some space at the bottom for now (In the future
    we'll likely add more height with additional copy so it's a temporary
    issue).

--- website/home/app/page.tsx
+++ website/home/app/page.tsx
@@ -386,6 +386,8 @@ const agent = new Agent({
         transition={{ duration: 0.8, delay: 0.8, ease: "easeOut" }}
         className="flex gap-6 flex-wrap items-center justify-center pb-8"
       ></motion.footer>
+      {/* Bottom spacer bar */}
+      <div className="w-full h-[600px] bg-gradient-to-b from-transparent to-black/5" />
     </div>
   );
 }
