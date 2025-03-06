6729feca9234796e2204ae5e59563ef727d9b49a
Derek Legenzoff <derek@cortexclick.com>
2025-03-06 14:38:09 -0800

    chore: adding llms.txt to sitemap (#435)
    Adds the llms.txt and llms-full.txt files to the sitemap

--- website/home/next-sitemap.config.js
+++ website/home/next-sitemap.config.js
@@ -3,6 +3,12 @@ module.exports = {
   siteUrl: "https://gensx.com",
   generateRobotsTxt: true,
   exclude: ["/blog/_*"],
+  additionalPaths: async () => {
+    return [
+      { loc: "/llms.txt", changefreq: "daily", priority: 0.7 },
+      { loc: "/llms-full.txt", changefreq: "daily", priority: 0.7 },
+    ];
+  },
   robotsTxtOptions: {
     additionalSitemaps: ["https://gensx.com/docs/sitemap.xml"],
   },
