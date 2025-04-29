76ec7c433149d006a159ccd1b3ee00bddb7bee1e
Jeremy Moseley <jeremy@gensx.com>
2025-04-29 08:53:14 -0700

    fix: Remove gating for deploy/environment/etc. (#642)
    ## Proposed changes
    
    Remove the gating for the `deploy` commands and others.
    
    
    https://discord.com/channels/1332417959032782908/1332417959473451071/1366803429590306846

--- packages/gensx/src/index.ts
+++ packages/gensx/src/index.ts
@@ -11,7 +11,6 @@ import { login } from "./commands/login.js";
 import { NewCommandOptions, newProject } from "./commands/new.js";
 import { runWorkflow } from "./commands/run.js";
 import { start } from "./commands/start.js";
-import { getAuth } from "./utils/config.js";
 import { VERSION } from "./utils/user-agent.js";
 
 export async function runCLI() {
@@ -49,97 +48,92 @@ export async function runCLI() {
     .option("-q, --quiet", "Suppress output", false)
     .action(start);
 
-  const auth = await getAuth();
+  program
+    .command("build")
+    .description("Build a GenSX project")
+    .argument(
+      "<file>",
+      "Workflow file to build (e.g. workflow.ts). This should export a gsx.Workflow as the default export.",
+    )
+    .option("-o, --out-dir <dir>", "Output directory")
+    .option("-t, --tsconfig <file>", "TypeScript config file")
+    .action(async (file: string, options: BuildOptions) => {
+      const outFile = await build(file, options);
+      console.info(`Workflow built to ${outFile.bundleFile}`);
+    });
 
-  // Only show Workflow execution commands to Gensx users for now.
-  if (auth && auth.org === "gensx") {
-    program
-      .command("build")
-      .description("Build a GenSX project")
-      .argument(
-        "<file>",
-        "Workflow file to build (e.g. workflow.ts). This should export a gsx.Workflow as the default export.",
-      )
-      .option("-o, --out-dir <dir>", "Output directory")
-      .option("-t, --tsconfig <file>", "TypeScript config file")
-      .action(async (file: string, options: BuildOptions) => {
-        const outFile = await build(file, options);
-        console.info(`Workflow built to ${outFile.bundleFile}`);
-      });
+  program
+    .command("deploy")
+    .description("Deploy a project to GenSX Cloud")
+    .argument("<file>", "File to deploy")
+    .option(
+      "-ev, --env-var <VALUE[=value]>",
+      "Environment variable to include with deployment (can be used multiple times)",
+      (val, prev: Record<string, string> = {}) => {
+        let [key, value] = val.split("=") as [string, string | undefined];
+        if (!key) {
+          throw new Error(
+            "Environment variables must be in the format KEY=value",
+          );
+        }
+        value ??= process.env[key];
+        if (value === undefined) {
+          throw new Error(`Environment variable ${key} has no value.`);
+        }
+        return { ...prev, [key]: value };
+      },
+      {},
+    )
+    .option("-p, --project <name>", "Project name to deploy to")
+    .option("-e, --env <name>", "Environment name to deploy to")
+    .action(deploy);
 
-    program
-      .command("deploy")
-      .description("Deploy a project to GenSX Cloud")
-      .argument("<file>", "File to deploy")
-      .option(
-        "-ev, --env-var <VALUE[=value]>",
-        "Environment variable to include with deployment (can be used multiple times)",
-        (val, prev: Record<string, string> = {}) => {
-          let [key, value] = val.split("=") as [string, string | undefined];
-          if (!key) {
-            throw new Error(
-              "Environment variables must be in the format KEY=value",
-            );
-          }
-          value ??= process.env[key];
-          if (value === undefined) {
-            throw new Error(`Environment variable ${key} has no value.`);
-          }
-          return { ...prev, [key]: value };
-        },
-        {},
-      )
-      .option("-p, --project <name>", "Project name to deploy to")
-      .option("-e, --env <name>", "Environment name to deploy to")
-      .action(deploy);
+  program
+    .command("run")
+    .description("Run a workflow")
+    .argument("<workflow>", "Workflow name")
+    .option("-i, --input <input>", "Input to pass to the workflow", "{}")
+    .option("--no-wait", "Do not wait for the workflow to finish")
+    .option("-p, --project <name>", "Project name to run the workflow in")
+    .option("-e, --env <name>", "Environment name to run the workflow in")
+    .option(
+      "-o, --output <file>",
+      "Output file to write the workflow result to",
+      undefined,
+    )
+    .action(runWorkflow);
 
-    program
-      .command("run")
-      .description("Run a workflow")
-      .argument("<workflow>", "Workflow name")
-      .option("-i, --input <input>", "Input to pass to the workflow", "{}")
-      .option("--no-wait", "Do not wait for the workflow to finish")
-      .option("-p, --project <name>", "Project name to run the workflow in")
-      .option("-e, --env <name>", "Environment name to run the workflow in")
-      .option(
-        "-o, --output <file>",
-        "Output file to write the workflow result to",
-        undefined,
-      )
-      .action(runWorkflow);
+  // Environment management commands
+  const environmentCommand = program
+    .command("env")
+    .description("Manage GenSX environments")
+    .action(handleShowEnvironment);
 
-    // Environment management commands
-    const environmentCommand = program
-      .command("env")
-      .description("Manage GenSX environments")
-      .action(handleShowEnvironment);
+  environmentCommand
+    .command("create")
+    .description("Create a new environment")
+    .argument("<name>", "Name of the environment")
+    .option("-p, --project <name>", "Project name")
+    .action(handleCreateEnvironment);
 
-    environmentCommand
-      .command("create")
-      .description("Create a new environment")
-      .argument("<name>", "Name of the environment")
-      .option("-p, --project <name>", "Project name")
-      .action(handleCreateEnvironment);
+  environmentCommand
+    .command("ls")
+    .description("List all environments")
+    .option("-p, --project <name>", "Project name")
+    .action(handleListEnvironments);
 
-    environmentCommand
-      .command("ls")
-      .description("List all environments")
-      .option("-p, --project <name>", "Project name")
-      .action(handleListEnvironments);
+  environmentCommand
+    .command("select")
+    .description("Select an environment as active")
+    .argument("<name>", "Name of the environment to select")
+    .option("-p, --project <name>", "Project name")
+    .action(handleSelectEnvironment);
 
-    environmentCommand
-      .command("select")
-      .description("Select an environment as active")
-      .argument("<name>", "Name of the environment to select")
-      .option("-p, --project <name>", "Project name")
-      .action(handleSelectEnvironment);
-
-    environmentCommand
-      .command("unselect")
-      .description("Unselect an environment")
-      .option("-p, --project <name>", "Project name")
-      .action(handleUnselectEnvironment);
-  }
+  environmentCommand
+    .command("unselect")
+    .description("Unselect an environment")
+    .option("-p, --project <name>", "Project name")
+    .action(handleUnselectEnvironment);
 
   await program.parseAsync();
 }
